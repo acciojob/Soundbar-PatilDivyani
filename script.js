543e957f687ddf072eab63e6435d29b302d45b44
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-29 15:33:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+let audios = [
+                "applause",
+                "boo",
+                "gasp",
+                "tada",
+                "victory",
+                "wrong"
+            ];
+
+
+const buttons = document.getElementsByTagName("button");
+let activeAudio = null;
+
+function playAudio() {
+	stopAudio();
+	activeAudio.play();
+}
+function stopAudio() {
+	if (activeAudio) {
+		activeAudio.pause();
+	}
+}
+
+for (let i = 0; i < buttons.length; i++) {
+	buttons.addEventListener("click", (event)=>{
+		//check if btn vaue == array.value
+		let innerText = event.target.innerText;
+		if(audios.includes(innerText)){
+			//play that music
+			let audioElement = document.createElement("audio");
+			audioElement.src = `.sound/+${innerText}+.mp3`;
+			audioElement.controls = true;
+			activeAudio = audioElement;
+			playAudio();
+		}
+		else{
+			stopAudio();
+		}
+	})
+}
+
+
